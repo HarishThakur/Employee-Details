26003615f639c219eafcad13db35de17ac09ca2d
HarishThakur <harish.thakur@soucebits.com>
2015-09-30 11:57:10 +0530

    Initial Commit

---
+++ Employee Details/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Employee Details
+//
+//  Created by Harish Singh on 30/09/15.
+//  Copyright (c) 2015 Sourcebits Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Employee Details/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Employee Details
+//
+//  Created by Harish Singh on 30/09/15.
+//  Copyright (c) 2015 Sourcebits Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
